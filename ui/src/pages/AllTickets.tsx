3ef9d03153bcc087baa8e4326d671643b923d9c5
Nimit Jain <jain.nimit99@gmail.com>
2025-06-08 20:19:40 +0530

    AllTickets null removed from <>

--- ui/src/pages/AllTickets.tsx
+++ ui/src/pages/AllTickets.tsx
@@ -1,6 +1,7 @@
-const AllTickets: React.FC<null> = () => {
+const AllTickets: React.FC<{}> = () => {
     return (
         <>
+            Hello
             {/* Search Bar */}
             {/* Filters */}
             {/* Ticket Cards List */}
