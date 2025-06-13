a2d00b70235362b6e4f618a051794deb569c7361
Nimit Jain <jain.nimit99@gmail.com>
2025-06-13 12:55:05 +0530

    fci-index.scss added

--- ui/src/index.tsx
+++ ui/src/index.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
 import './index.css';
+import './fci-index.scss'
 import './index.scss';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'antd/dist/reset.css'; // for antd v5+
