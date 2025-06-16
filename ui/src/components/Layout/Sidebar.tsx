89c2b863ed6b4d62f6231cb6b9c00f3479ecac82
Nimit Jain <53301697+nimit-27@users.noreply.github.com>
2025-06-16 15:20:59 +0530

    Add Escalation Master page and sidebar option

--- ui/src/components/Layout/Sidebar.tsx
+++ ui/src/components/Layout/Sidebar.tsx
@@ -6,6 +6,8 @@ import AddCircleOutlineIcon from '@mui/icons-material/AddCircleOutline';
 import LibraryBooksIcon from '@mui/icons-material/LibraryBooks';
 import CategoryIcon from '@mui/icons-material/Category';
 import CustomIconButton from '../UI/IconButton/CustomIconButton';
+import { currentUserDetails } from '../../config/config';
+import SupervisorAccountIcon from '@mui/icons-material/SupervisorAccount';
 
 const menuItems = [
   {
@@ -27,6 +29,12 @@ const menuItems = [
     label: 'Categories Master',
     to: '/categories-master',
     icon: <CategoryIcon />,
+  },
+  {
+    label: 'Escalation Master',
+    to: '/escalation-master',
+    icon: <SupervisorAccountIcon />,
+    roles: ['ADMIN', 'IT'],
   },
 ];
 
@@ -53,12 +61,19 @@ const Sidebar: React.FC = () => {
       </div>
 
       <List component="nav">
-        {menuItems.map(({ label, to, icon }) => (
-          <ListItemButton component={Link} to={to} key={label}>
-            <ListItemIcon className='text-white'>{icon}</ListItemIcon>
-            {!collapsed && <ListItemText className='text-white fs-1' primaryTypographyProps={{ fontSize: '1.2rem' }} primary={label} />}
-          </ListItemButton>
-        ))}
+        {menuItems.map(({ label, to, icon, roles }) => {
+          if (roles && !roles.some(r => currentUserDetails.role.includes(r))) {
+            return null;
+          }
+          return (
+            <ListItemButton component={Link} to={to} key={label}>
+              <ListItemIcon className='text-white'>{icon}</ListItemIcon>
+              {!collapsed && (
+                <ListItemText className='text-white fs-1' primaryTypographyProps={{ fontSize: '1.2rem' }} primary={label} />
+              )}
+            </ListItemButton>
+          );
+        })}
       </List>
     </div>
   );
