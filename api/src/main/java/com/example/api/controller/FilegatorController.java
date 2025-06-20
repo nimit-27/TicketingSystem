de819d318285448e5eec6d93f3e846a4a35a09b9
Nimit Jain <jain.nimit99@gmail.com>
2025-06-20 15:48:37 +0530

    allowCredentials added

--- api/src/main/java/com/example/api/controller/FilegatorController.java
+++ api/src/main/java/com/example/api/controller/FilegatorController.java
@@ -14,7 +14,7 @@ import java.util.List;
 
 @RestController
 @RequestMapping("/filegator")
-@CrossOrigin(origins = "http://localhost:3000")
+@CrossOrigin(origins = "http://localhost:3000", allowCredentials = "true")
 public class FilegatorController {
 
     @PostMapping("/login")
