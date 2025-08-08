31757466f5f76c2c10ca53f3089f40bff36deb3a
Nimit Jain <jain.nimit99@gmail.com>
2025-08-08 12:08:57 +0530

    Pagination reverted back

--- ui/src/components/PaginationControls.tsx
+++ ui/src/components/PaginationControls.tsx
@@ -9,30 +9,7 @@ interface PaginationControlsProps {
 }
 
 const PaginationControls: React.FC<PaginationControlsProps> = ({ page, totalPages, onChange, className }) => (
-    <Pagination
-        count={totalPages}
-        page={page}
-        onChange={onChange}
-        className={className}
-        color="primary"
-        boundaryCount={3}
-        siblingCount={0}
-        renderItem={(item) => {
-            if (item.type === 'page') {
-                if (item.page <= 3 || item.page === totalPages) {
-                    return <PaginationItem {...item} />;
-                }
-                if (item.page === 4) {
-                    return <PaginationItem {...item} type="end-ellipsis" />;
-                }
-                return null;
-            }
-            if (item.type === 'end-ellipsis' || item.type === 'start-ellipsis') {
-                return null;
-            }
-            return <PaginationItem {...item} />;
-        }}
-    />
+    <Pagination count={totalPages} page={page} onChange={onChange} className={className} color="primary" />
 );
 
 export default PaginationControls;
