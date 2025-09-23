caf6b4315ad71cff5ce482d69379f5314132e700
Nimit Jain <53301697+nimit-27@users.noreply.github.com>
2025-09-22 22:45:39 -0700

    Ensure notification repository is discovered

--- api/src/main/java/com/example/api/Main.java
+++ api/src/main/java/com/example/api/Main.java
@@ -2,16 +2,19 @@ package com.example.api;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication(scanBasePackages = "com.example")
 @ConfigurationPropertiesScan(basePackages = "com.example")
+@EnableJpaRepositories(basePackages = "com.example.notification.repository")
+@EntityScan(basePackages = "com.example.notification.models")
 //@EnableScheduling
 public class Main {
 
-	public static void main(String[] args) {
-		SpringApplication.run(Main.class, args);
-	}
-
+    public static void main(String[] args) {
+        SpringApplication.run(Main.class, args);
+    }
 }
