d58ff2ea11e9accb7017f03cb8fc6c57e0b7b703
Nimit Jain <53301697+nimit-27@users.noreply.github.com>
2025-09-24 10:20:11 +0530

    Handle SSE responses in ApiResponse advice

--- api/src/main/java/com/example/api/config/ApiResponseBodyAdvice.java
+++ api/src/main/java/com/example/api/config/ApiResponseBodyAdvice.java
@@ -9,6 +9,7 @@ import org.springframework.http.server.ServerHttpResponse;
 import org.springframework.http.server.ServletServerHttpResponse;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseBodyAdvice;
+import org.springframework.web.servlet.mvc.method.annotation.SseEmitter;
 
 @RestControllerAdvice
 public class ApiResponseBodyAdvice implements ResponseBodyAdvice {
@@ -19,7 +20,15 @@ public class ApiResponseBodyAdvice implements ResponseBodyAdvice {
 
     @Override
     public Object beforeBodyWrite(Object body, MethodParameter returnType, MediaType selectedContentType, Class selectedConverterType, ServerHttpRequest request, ServerHttpResponse response) {
-        if (body instanceof ApiResponse) return body;
+        if (body instanceof ApiResponse) {
+            return body;
+        }
+        if (body instanceof SseEmitter) {
+            return body;
+        }
+        if (selectedContentType != null && MediaType.TEXT_EVENT_STREAM.isCompatibleWith(selectedContentType)) {
+            return body;
+        }
         if (body == null && response instanceof ServletServerHttpResponse) {
             ServletServerHttpResponse servletResponse = (ServletServerHttpResponse) response;
             if (servletResponse.getServletResponse().getStatus() == HttpStatus.CREATED.value()) {
