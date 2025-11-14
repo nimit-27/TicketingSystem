dac48c6980146a0f0349f15b181b47f2403892a0
Nimit <jain.nimit99@gmail.com>
2025-11-14 13:39:17 +0530

    dump -U

--- db/ticketing_system_dump (2).sql
+++ db/ticketing_system_dump (2).sql
@@ -1371,4 +1371,4 @@ SET character_set_client = @saved_cs_client;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2025-11-14 13:38:42
+-- Dump completed on 2025-11-14 13:38:11
