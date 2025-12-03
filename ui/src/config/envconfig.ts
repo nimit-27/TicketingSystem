675459ab1d076360c5506c4dd83b7671bb096693
Nimit <jain.nimit99@gmail.com>
2025-12-04 03:05:47 +0530

    devmode false

--- ui/src/config/envconfig.ts
+++ ui/src/config/envconfig.ts
@@ -1,6 +1,6 @@
 export const envConfig = {
     Roles: ["L1", "L2", "L3", "ADMIN", "FCI_USER", "USER", "RNO", "HELPDESK"],
-    devMode: true,
+    devMode: false,
     FciTheme: true,
     filegatorEnabled: false,
     Users: {
