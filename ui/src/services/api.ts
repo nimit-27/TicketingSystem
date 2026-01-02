afc1ff4aa9e51f2c0668f178d1e5c9eefdaad096
Nimit <jain.nimit99@gmail.com>
2026-01-02 18:49:59 +0530

    + stg env config

--- ui/src/services/api.ts
+++ ui/src/services/api.ts
@@ -1,6 +1,7 @@
-export const BASE_URL = 'http://localhost:8082/helpdesk';
+// export const BASE_URL = 'http://localhost:8082/helpdesk';
 // export const BASE_URL = 'https://dev.annadarpan.in/helpdeskbackend';
 // export const BASE_URL = 'https://uat.annadarpan.in/helpdeskbackend';
+export const BASE_URL = 'https://stg.annadarpan.in/helpdeskbackend';
 // export const BASE_URL = process.env.REACT_APP_API_URL || 'https://dev.annadarpan.in/helpdeskbackend';
 
 export const ANNADARPAN_KEYCLOAK_URL = 'https://devkeycloak.annadarpan.in/realms/AnnaDarpan'
