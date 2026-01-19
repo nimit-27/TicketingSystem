6294ddf5f40bf3944204bf0160ffb514e01501cc
Nimit Jain <53301697+nimit-27@users.noreply.github.com>
2026-01-19 17:41:40 +0530

    Expand external SSO token response fields

--- api/src/main/java/com/ticketingSystem/api/dto/ExternalSsoTokenResponse.java
+++ api/src/main/java/com/ticketingSystem/api/dto/ExternalSsoTokenResponse.java
@@ -2,6 +2,7 @@ package com.ticketingSystem.api.dto;
 
 import com.fasterxml.jackson.annotation.JsonAlias;
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.Getter;
 import lombok.Setter;
 
@@ -14,4 +15,23 @@ public class ExternalSsoTokenResponse {
 
     @JsonAlias({"expires_in", "expiresIn"})
     private Long expiresIn;
+
+    @JsonAlias({"refresh_expires_in", "refreshExpiresIn"})
+    private Long refreshExpiresIn;
+
+    @JsonAlias({"refresh_token", "refreshToken"})
+    private String refreshToken;
+
+    @JsonAlias({"token_type", "tokenType"})
+    private String tokenType;
+
+    @JsonProperty("not-before-policy")
+    @JsonAlias({"not_before_policy", "notBeforePolicy"})
+    private Long notBeforePolicy;
+
+    @JsonAlias({"session_state", "sessionState"})
+    private String sessionState;
+
+    @JsonAlias({"scope"})
+    private String scope;
 }
