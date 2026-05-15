aaf2ac3129bfedfc093d5115c5c52e7352ce914c
Nimit <jain.nimit99@gmail.com>
2026-05-15 12:30:11 +0530

    added @Repository

--- api/src/main/java/com/ticketingSystem/reportGenerator/repository/ReportFilterMappingRepository.java
+++ api/src/main/java/com/ticketingSystem/reportGenerator/repository/ReportFilterMappingRepository.java
@@ -2,9 +2,11 @@ package com.ticketingSystem.reportGenerator.repository;
 
 import com.ticketingSystem.reportGenerator.models.ReportFilterMapping;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
+@Repository
 public interface ReportFilterMappingRepository extends JpaRepository<ReportFilterMapping, Long> {
     List<ReportFilterMapping> findByReport_ReportIdOrderByDisplayOrderAsc(Long reportId);
 }
