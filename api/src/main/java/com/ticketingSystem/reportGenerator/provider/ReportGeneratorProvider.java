efb56675022358e66e2ab3f4ec3e109250ac8404
Nimit <jain.nimit99@gmail.com>
2026-05-15 16:11:33 +0530

    made ReportGeneratorProvider generic

--- api/src/main/java/com/ticketingSystem/reportGenerator/provider/ReportGeneratorProvider.java
+++ api/src/main/java/com/ticketingSystem/reportGenerator/provider/ReportGeneratorProvider.java
@@ -5,17 +5,18 @@ import com.ticketingSystem.reportGenerator.enums.ReportFormat;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.ApplicationContext;
 import org.springframework.stereotype.Component;
+import org.springframework.util.StringUtils;
 
 @Component
 public class ReportGeneratorProvider {
-    @Value("${report.engine:jasper}")
+    @Value("${report.engine:default}")
     private String reportEngine;
 
-    @Value("${report.generate.jasper.pdf:jasperPdf}")
-    private String jasperPdfBeanName;
+    @Value("${report.generate.default.pdf:}")
+    private String defaultPdfBeanName;
 
-    @Value("${report.generate.jasper.excel:jasperExcel}")
-    private String jasperExcelBeanName;
+    @Value("${report.generate.default.excel:}")
+    private String defaultExcelBeanName;
 
     private final ApplicationContext ctx;
 
@@ -24,14 +25,27 @@ public class ReportGeneratorProvider {
     }
 
     public ReportGenerator getGenerator(ReportFormat format) {
-        String engine = reportEngine == null ? "jasper" : reportEngine.trim().toLowerCase();
-        if (!"jasper".equals(engine)) {
-            throw new IllegalArgumentException("Unsupported report.engine: " + reportEngine);
+        String engine = StringUtils.hasText(reportEngine) ? reportEngine.trim().toLowerCase() : "default";
+
+        String beanNameFromEngineAndFormat = "report.generate." + engine + "." + format.name().toLowerCase();
+
+        String beanName = ctx.getEnvironment().getProperty(beanNameFromEngineAndFormat);
+
+        if(!StringUtils.hasText(beanName)) {
+            beanName = switch (format) {
+                case PDF -> defaultPdfBeanName;
+                case EXCEL -> defaultExcelBeanName;
+            };
         }
 
-        return switch (format) {
-            case PDF -> ctx.getBean(jasperPdfBeanName, ReportGenerator.class);
-            case EXCEL -> ctx.getBean(jasperExcelBeanName, ReportGenerator.class);
-        };
+        if(!StringUtils.hasText(beanName)) {
+            throw new IllegalArgumentException("No report generator bean configured for an engine=" + engine + ", format=" + format);
+        }
+
+        if(ctx.containsBean(beanName)) {
+            throw new IllegalArgumentException("Configured report generator bean not found: " + beanName);
+        }
+
+        return ctx.getBean(beanName, ReportGenerator.class);
     }
 }
