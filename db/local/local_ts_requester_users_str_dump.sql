3f441d1c019832bf2fee9efc362d941c7f5d45a5
Nimit <jain.nimit99@gmail.com>
2026-07-09 00:06:11 +0530

    data dump, queries -U

---
+++ db/local/local_ts_requester_users_str_dump.sql
@@ -0,0 +1,60 @@
+-- MySQL dump 10.13  Distrib 8.0.42, for Win64 (x86_64)
+--
+-- Host: 127.0.0.1    Database: ticketing_system
+-- ------------------------------------------------------
+-- Server version	8.0.41
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!50503 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `requester_users_backup`
+--
+
+DROP TABLE IF EXISTS `requester_users_backup`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `requester_users_backup` (
+  `requester_user_id` varchar(100) NOT NULL,
+  `name` varchar(100) DEFAULT NULL,
+  `email_id` varchar(100) DEFAULT NULL,
+  `mobile_no` varchar(15) DEFAULT NULL,
+  `office` varchar(100) DEFAULT NULL,
+  `username` varchar(50) DEFAULT NULL,
+  `password` varchar(255) DEFAULT NULL,
+  `roles` varchar(255) DEFAULT NULL,
+  `stakeholder` varchar(50) DEFAULT NULL,
+  `first_name` varchar(100) DEFAULT NULL,
+  `middle_name` varchar(100) DEFAULT NULL,
+  `last_name` varchar(100) DEFAULT NULL,
+  `date_of_joining` datetime DEFAULT NULL,
+  `date_of_retirement` datetime DEFAULT NULL,
+  `office_type` varchar(20) DEFAULT NULL,
+  `office_code` varchar(20) DEFAULT NULL,
+  `zone_code` varchar(2) DEFAULT NULL,
+  `district_code` varchar(6) DEFAULT NULL,
+  `region_code` varchar(3) DEFAULT NULL,
+  `active` tinyint(1) DEFAULT NULL,
+  PRIMARY KEY (`requester_user_id`),
+  UNIQUE KEY `requester_username` (`username`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2026-07-02 10:52:47
---
+++ db/queries/query_to_get_sla_breach_3.sql
@@ -0,0 +1,17 @@
+SELECT 
+	t.ticket_id AS 'Ticket Id', 
+	t.reported_date AS 'Reported Date',
+    itm.name,
+    t.status AS 'Current Status',
+    t.last_modified_status_date AS 'Status Updated Date',
+    ts.due_at AS 'Due At',
+    CASE
+		WHEN ts.breached_by_minutes > 0
+			THEN 'YES'
+		ELSE 'NO'
+	END AS 'Is Breached'
+    FROM ad_prd_ticket_system.ticket_sla ts
+JOIN tickets t
+	ON t.ticket_id = ts.ticket_id
+JOIN issue_type_master itm
+	ON itm.issue_type_id = t.issue_type_id;
